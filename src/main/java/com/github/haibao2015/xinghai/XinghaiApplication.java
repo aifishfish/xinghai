f71e6c423c5de7cd815121db93232b9fa800be7f
wuque <swchenminglei@163.com>
2023-08-11 16:47:34 +0800

    init

---
+++ src/main/java/com/github/haibao2015/xinghai/XinghaiApplication.java
@@ -0,0 +1,13 @@
+package com.github.haibao2015.xinghai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinghaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinghaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/haibao2015/xinghai/XinghaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.haibao2015.xinghai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinghaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
